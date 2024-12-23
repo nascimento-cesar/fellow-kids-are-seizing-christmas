6f75be78f18115adbe44c22d8dab8ffdbaa04165
nascimento-cesar <166350032+nascimento-cesar@users.noreply.github.com>
2024-12-23 18:09:55 -0300

    feat: create start screen

--- src/screens/start.lua
+++ src/screens/start.lua
@@ -1,6 +1,11 @@
 StartScreen = {}
 
 function StartScreen:draw()
-  cls(1)
-  Draw:print_text_bouncy("start screen", Draw:get_offset(), Draw:get_offset(), { 7, 6 }, 0)
+  local text_1, text_2, text_3, text_4 = "fellow kids", "are seizing", "christmas", "press ❎ or 🅾️ to start"
+  cls(3)
+
+  Draw:print_text(text_1, Draw:get_h_center(text_1), Draw:get_offset(4), 7, 0)
+  Draw:print_text_bouncy(text_2, Draw:get_h_center(text_2), Draw:get_offset(6), { 14, 8 }, 0)
+  Draw:print_text(text_3, Draw:get_h_center(text_3), Draw:get_offset(8), 11, 0)
+  Draw:print_text(text_4, Draw:get_h_center(text_4), Draw:get_offset(12), { 7, 5 }, 0)
 end
