c6924120141dca3f178f1fde9c2cf9efcf1c8626
nascimento-cesar <166350032+nascimento-cesar@users.noreply.github.com>
2024-12-23 22:50:10 -0300

    feat: create arrow hint

--- src/screens/gameplay.lua
+++ src/screens/gameplay.lua
@@ -4,21 +4,29 @@ function GameplayScreen:draw(gameplay)
   Draw:draw_map()
   self:draw_health(gameplay)
   self:draw_score(gameplay)
+  self:draw_arrows()
   self:draw_gifts(gameplay)
   self:draw_santa(gameplay)
   self:draw_kids(gameplay.kids)
   self:draw_kids(gameplay.gifted_kids)
 end
 
-function GameplayScreen:draw_health(gameplay)
-  for i = 1, gameplay.santa.health do
-    Draw:draw_sprite(104, Draw:get_offset(5) + (i - 1) * 12, Draw:get_offset(14))
-  end
+function GameplayScreen:draw_arrows(gameplay)
+  Draw:print_text("⬅️", Draw:get_offset(10), Draw:get_offset(2) + 1, btn(⬅️) and 7 or 4, 0)
+  Draw:print_text("⬇️", Draw:get_offset(11) + 2, Draw:get_offset(2) + 1, btn(⬇️) and 7 or 3, 0)
+  Draw:print_text("➡️", Draw:get_offset(12) + 4, Draw:get_offset(2) + 1, btn(➡️) and 7 or 8, 0)
+  Draw:print_text("⬆️", Draw:get_offset(11) + 2, Draw:get_offset(), btn(⬆️) and 7 or 10, 0)
 end
 
 function GameplayScreen:draw_gifts(gameplay)
   for gift in all(gameplay.gifts) do
     Draw:draw_sprite(Draw:get_gift_sprite(gift.kid.gift_index), gift.x, gift.y, 2, 2)
+  end
+end
+
+function GameplayScreen:draw_health(gameplay)
+  for i = 1, gameplay.santa.health do
+    Draw:draw_sprite(104, Draw:get_offset(5) + (i - 1) * 12, Draw:get_offset(14))
   end
 end
 
@@ -45,5 +53,5 @@ end
 
 function GameplayScreen:draw_score(gameplay)
   local text = "score: " .. gameplay.score
-  Draw:print_text(text, Draw:get_offset(6), Draw:get_offset(), 7, true)
+  Draw:print_text(text, Draw:get_offset(1), Draw:get_offset(), 7, true)
 end
